02ef67714255674fa5a4acfa59a1051510c529a5
shiba0627 <1e8ec3@gmail.com>
2025-08-26 16:05:59 +0900

    v1.4

--- gui-package/basebutton.py
+++ gui-package/basebutton.py
@@ -9,10 +9,35 @@ class BaseButton:
         self.name = name
         print(BUTTONS['forward']['normal'])
 
-def main():
-    print('test')
-    a = BaseButton()
+class GUIapp():
+    def __init__(self):
+        self.root = tk.Tk()
+        self.root.state('zoomed')#ウィンドウ最大化
+        self.root.update_idletasks()#ウィンドウ表示待ち
 
+        # ウィンドウサイズを取得
+        self.width = self.root.winfo_width()
+        self.height = self.root.winfo_height()
+
+        self.canvas = tk.Canvas(self.root, width = self.width, height = self.height)
+        self.canvas.pack(fill='both', expand=True)
+
+        button_list = [
+            ('forward', 1/2, 1/6, 'w'),
+            ('ccw', 1/4, 1/2, 'a'),
+            ('cw', 3/4, 1/2, 'd'),
+            ('stop', 1/2, 1/2, 's'),
+            ('back', 1/2, 5/6, 'z')
+        ]
+
+        self.buttons = []#各ボタンのオブジェクトを格納するリスト
+        for name, center_x_ratio, center_y_ratio, cmd in button_list:
+            
+
+
+
+def main():
+    app = GUIapp()
 
 if __name__ == '__main__':
     main()
