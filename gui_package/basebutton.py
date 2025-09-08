3e0689ab9735223c79153ed4a0d5db27773d2b9c
shiba0627 <1e8ec3@gmail.com>
2025-09-08 23:06:32 +0900

    v1.7

--- gui_package/basebutton.py
+++ gui_package/basebutton.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+from PIL import Image, ImageTk
 from config.filepath import CW, CW_ACTIVE, CW_ATTENTION, CW_LOCK
 from config.filepath import CCW, CCW_ACTIVE, CCW_ATTENTION, CW_LOCK
 from config.filepath import BUTTONS
@@ -6,8 +7,13 @@ from config.static import BUTTON_SIZE_RATIO
 from typing import TypedDict
 
 class BaseButton:
-    def __init__(self, name = None, area = None, cmd = None):
-        self.name = name
+    def __init__(self, canvas, name = None, area = None, cmd = None):
+        self.name = name#ボタンの種類
+        self.canvas = canvas
+        width = int(area[2] - area[0])
+        height = int(area[3] - area[1])
+        self.img = ImageTk.PhotoImage(Image.open(BUTTONS[self.name]['normal']).resize((width, height)))
+
         print(BUTTONS['forward']['normal'])
 
 class GUIapp():
@@ -31,14 +37,21 @@ class GUIapp():
             ('back', 1/2, 5/6, 'z')
         ]
 
-        self.button_seze_ratio = 0.13 
         self.buttons = []#各ボタンのオブジェクトを格納するリスト
         for name, center_x_ratio, center_y_ratio, cmd in button_list:
             center_x = center_x_ratio * self.width
             center_y = center_y_ratio * self.height
+            area = self._calc_area(center_x, center_y)
         print(f'width = {self.width}, height = {self.height}')
-    def _calc_area(button_size_ratio, ):
-
+    
+    def _calc_area(self, center_x, center_y):
+        button_size = BUTTON_SIZE_RATIO * self.width
+        area = (center_x - button_size/2,
+                center_y - button_size/2,
+                center_x + button_size/2,
+                center_y + button_size/2)
+        return area
+        
 
 
 def main():
